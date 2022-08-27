5ed66db4ad27ad20b02fdb3df77b90fa595ad9e8
muing <ungetqqq@gmail.com>
2022-08-27 22:40:28 +0900

    :hammer: Exeption fixed.

--- 06_QDateAndQDateEdit/widget.cpp
+++ 06_QDateAndQDateEdit/widget.cpp
@@ -11,30 +11,47 @@ Widget::Widget(QWidget *parent)
 
     setFixedSize(500, 500);
 
+    dateEdit[0] = NULL;
+    dateEdit[1] = NULL;
+    dateLabel[0] = NULL;
+    dateLabel[1] = NULL;
+
     date[0] = QDate(2022, 3, 9);
     date[1] = QDate::currentDate();
 
     dateEdit[0] = new QDateEdit(date[0], this);
+    if (dateEdit[0] == NULL)
+        throw "Error: QDateEdit allocation fail.";
     dateEdit[0]->setGeometry(10, 10, 140, 40);
     connect(dateEdit[0], SIGNAL(dateChanged(QDate)), SLOT(Date1Fixed(QDate)));
 
     dateEdit[1] = new QDateEdit(date[1], this);
+    if (dateEdit[1] == NULL)
+        throw "Error: QDateEdit allocation fail.";
     dateEdit[1]->setGeometry(10, 50, 140, 40);
     connect(dateEdit[1], SIGNAL(dateChanged(QDate)), SLOT(Date2Fixed(QDate)));
 
     dateLabel[0] = new QLabel(date[0].toString(Qt::ISODate), this);
+    if (dateLabel[0] == NULL)
+        throw "Error: QLabel allocation fail.";
     dateLabel[0]->setGeometry(10, 300, 200, 30);
 
     dateLabel[1] = new QLabel(date[1].toString(Qt::ISODate), this);
+    if (dateLabel[1] == NULL)
+        throw "Error: QLabel allocation fail.";
     dateLabel[1]->setGeometry(10, 350, 200, 30);
 }
 
 Widget::~Widget()
 {
-    delete dateEdit[0];
-    delete dateEdit[1];
-    delete dateLabel[0];
-    delete dateLabel[1];
+    if (dateEdit[0] != NULL)
+        delete dateEdit[0];
+    if (dateEdit[1] != NULL)
+        delete dateEdit[1];
+    if (dateLabel[0] != NULL)
+        delete dateLabel[0];
+    if (dateLabel[1] != NULL)
+        delete dateLabel[1];
     delete ui;
 }
 
